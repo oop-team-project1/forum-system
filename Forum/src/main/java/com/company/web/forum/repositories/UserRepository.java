19fca4a03632ee0c92105e56390b3e5a17be5a5d
Kaloyan <kaloyan.enchev@abv.bg>
2024-01-25 15:43:37 +0200

    UserRepository-Interface and Impl

--- Forum/src/main/java/com/company/web/forum/repositories/UserRepository.java
+++ Forum/src/main/java/com/company/web/forum/repositories/UserRepository.java
@@ -6,7 +6,7 @@ import com.company.web.forum.models.User;
 import java.util.List;
 
 public interface UserRepository {
-    List<User> getAll(FilterOptions filterOptions);
+    List<User> getAll();
     User getById(int id);
     User getByUsername(String username);
     User getByEmail(String email);
--- Forum/src/main/java/com/company/web/forum/repositories/UserRepositoryImpl.java
+++ Forum/src/main/java/com/company/web/forum/repositories/UserRepositoryImpl.java
@@ -1,4 +1,106 @@
 package com.company.web.forum.repositories;
 
-public class UserRepositoryImpl {
+import com.company.web.forum.exceptions.EntityNotFoundException;
+import com.company.web.forum.helpers.FilterOptions;
+import com.company.web.forum.models.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UserRepositoryImpl implements UserRepository
+{
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public UserRepositoryImpl(SessionFactory sessionFactory)
+    {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public List<User> getAll()
+    {
+        try (Session session = sessionFactory.openSession())
+        {
+            Query<User> query = session.createQuery("from User", User.class);
+            return query.list();
+        }
+    }
+
+    @Override
+    public User getById(int id)
+    {
+        try (Session session = sessionFactory.openSession())
+        {
+            User user = session.get(User.class, id);
+            if (user == null)
+            {
+                throw new EntityNotFoundException("User", id);
+            }
+            return user;
+        }
+    }
+
+    @Override
+    public User getByUsername(String username) {
+        try (Session session = sessionFactory.openSession())
+        {
+            Query<User> query = session.createQuery(
+                    "from User where username = :username", User.class);
+            query.setParameter("username", username);
+
+            List<User> result = query.list();
+            if (result.isEmpty())
+            {
+                throw new EntityNotFoundException("User", "username", username);
+            }
+
+            return result.get(0);
+        }
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        try (Session session = sessionFactory.openSession())
+        {
+            Query<User> query = session.createQuery(
+                    "from User where email = :email", User.class);
+            query.setParameter("email", email);
+
+            List<User> result = query.list();
+            if (result.isEmpty())
+            {
+                throw new EntityNotFoundException("User", "email", email);
+            }
+
+            return result.get(0);
+        }
+    }
+
+    @Override
+    public void create(User user)
+    {
+        try (Session session = sessionFactory.openSession())
+        {
+            session.beginTransaction();
+            session.persist(user);
+            session.getTransaction().commit();
+        }
+    }
+
+    @Override
+    public void update(User user)
+    {
+        try (Session session = sessionFactory.openSession())
+        {
+            session.beginTransaction();
+            session.merge(user);
+            session.getTransaction().commit();
+        }
+    }
 }
