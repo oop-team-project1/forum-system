3a67696e2c027545fd92984e0a4279f5fcdfa848
ivonasto <ivona.stoyanova.s@gmail.com>
2024-01-26 12:34:44 +0200

    Interface methods

--- Forum/src/main/java/com/company/web/forum/repositories/PostRepository.java
+++ Forum/src/main/java/com/company/web/forum/repositories/PostRepository.java
@@ -1,7 +1,16 @@
 package com.company.web.forum.repositories;
 
 
+import com.company.web.forum.helpers.FilterOptionsPosts;
+import com.company.web.forum.models.Post;
+import com.company.web.forum.models.User;
 
+import java.util.List;
 
 public interface PostRepository {
+    List<Post> getAll(FilterOptionsPosts filterOptions);
+    Post get(int id);
+    Post create(Post post, User user);
+    Post update(int id,User user);
+    void delete(int id);
 }
