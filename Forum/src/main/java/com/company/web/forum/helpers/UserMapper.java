b0e6a1ab9ab3d3d43ac1f973d2ec1361b9e60d64
Kaloyan <kaloyan.enchev@abv.bg>
2024-01-26 12:58:41 +0200

    UserDto-Mapper added

---
+++ Forum/src/main/java/com/company/web/forum/helpers/UserMapper.java
@@ -0,0 +1,39 @@
+package com.company.web.forum.helpers;
+
+import com.company.web.forum.models.User;
+import com.company.web.forum.models.UserDto;
+import com.company.web.forum.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper
+{
+    private final UserService userService;
+
+    @Autowired
+    public UserMapper(UserService userService)
+    {
+        this.userService = userService;
+    }
+
+    public User fromDto(int id, UserDto userDto)
+    {
+        User user = fromDto(userDto);
+        user.setId(id);
+        user.setAdmin(false);
+        return user;
+    }
+
+    public User fromDto(UserDto userDto)
+    {
+        User user = new User();
+        user.setFirstName(userDto.getFirstName());
+        user.setLastName(userDto.getLastName());
+        user.setUsername(userDto.getUsername());
+        user.setPassword(userDto.getPassword());
+        user.setEmail(userDto.getEmail());
+        user.setPhoneNumber(userDto.getPhoneNumber());
+        return user;
+    }
+}
