3de93f5c344df813886f0a37a1ce394545b26e0c
Kaloyan <kaloyan.enchev@abv.bg>
2024-01-26 12:21:36 +0200

    FilterOptionsUsers added

--- Forum/src/main/java/com/company/web/forum/helpers/FilterOptionsUsers.java
+++ Forum/src/main/java/com/company/web/forum/helpers/FilterOptionsUsers.java
@@ -1,4 +1,49 @@
 package com.company.web.forum.helpers;
 
-public class FilterOptionsUsers {
+import java.util.Optional;
+
+public class FilterOptionsUsers
+{
+    private Optional<String> username;
+    private Optional<String> firstName;
+    private Optional<String> lastName;
+    private Optional<String> email;
+    private Optional<String> sortBy;
+    private Optional<String> sortOrder;
+
+    public FilterOptionsUsers(String username, String firstName,
+                              String lastName, String email,
+                              String sortBy, String sortOrder)
+    {
+        this.username = Optional.ofNullable(username);
+        this.firstName = Optional.ofNullable(firstName);
+        this.lastName = Optional.ofNullable(lastName);
+        this.email = Optional.ofNullable(email);
+        this.sortBy = Optional.ofNullable(sortBy);
+        this.sortOrder = Optional.ofNullable(sortOrder);
+    }
+
+    public Optional<String> getUsername() {
+        return username;
+    }
+
+    public Optional<String> getFirstName() {
+        return firstName;
+    }
+
+    public Optional<String> getLastName() {
+        return lastName;
+    }
+
+    public Optional<String> getEmail() {
+        return email;
+    }
+
+    public Optional<String> getSortBy() {
+        return sortBy;
+    }
+
+    public Optional<String> getSortOrder() {
+        return sortOrder;
+    }
 }
