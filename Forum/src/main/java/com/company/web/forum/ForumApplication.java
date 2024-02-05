17e2f62365e0164f140276e164e110708d7583e9
Aleksandrina Velikova <avelikova@tu-sofia.bg>
2024-02-05 23:06:00 +0200

    added swagger dependency

--- Forum/src/main/java/com/company/web/forum/ForumApplication.java
+++ Forum/src/main/java/com/company/web/forum/ForumApplication.java
@@ -1,9 +1,12 @@
 package com.company.web.forum;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "TastyTale API", version = "1.0.0"))
 public class ForumApplication {
 
     public static void main(String[] args)
