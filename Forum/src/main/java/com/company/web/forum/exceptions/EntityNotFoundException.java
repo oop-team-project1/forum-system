4864e35e7b69a7fb6654d8408a1364a8e0827749
ivonasto <ivona.stoyanova.s@gmail.com>
2024-02-10 14:28:18 +0200

    EntityNotFoundException update to include open message

--- Forum/src/main/java/com/company/web/forum/exceptions/EntityNotFoundException.java
+++ Forum/src/main/java/com/company/web/forum/exceptions/EntityNotFoundException.java
@@ -8,4 +8,8 @@ public class EntityNotFoundException extends RuntimeException {
     public EntityNotFoundException(String type, String attribute, String value) {
         super(String.format("%s with %s %s not found.", type, attribute, value));
     }
+
+    public EntityNotFoundException(String message){
+        super(message);
+    }
 }
