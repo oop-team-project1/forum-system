d8026495c6de82d61e44979f911d0c36f090f7ed
Aleksandrina Velikova <avelikova@tu-sofia.bg>
2024-02-18 13:08:47 +0200

    added authorization for delete multiple posts by user

--- Forum/src/main/java/com/company/web/forum/controllers/mvc/PostMvcController.java
+++ Forum/src/main/java/com/company/web/forum/controllers/mvc/PostMvcController.java
@@ -19,6 +19,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Set;
 
 
@@ -473,6 +474,5 @@ public class PostMvcController {
             return "ErrorView";
         }
     }
-
 }
 
